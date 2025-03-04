203063a53ad8fdeaf6891486739a3114dad4bf94
alejandro.caceres.dominguez <alejandro.caceres.dominguez@auleseebe.upc.edu>
2025-03-04 09:24:05 +0100

    problema 2

--- Problema2.R
+++ Problema2.R
@@ -1,45 +1,38 @@
 #Codigo para problema 2
 mis_dades <- iris
-mis_dades
-dim(mis_dades)
-names(mis_dades)
-
-mean(mis_dades$Petal.Length)
-sd(mis_dades$Petal.Length)
-hist(mis_dades$Petal.Length)
-
 x <- mis_dades$Petal.Length
-y  <- mis_dades$Sepal.Length
+mean(x)
+sd(x)
+hist(x)
+
+y <- mis_dades$Sepal.Length
+mean(y)
+
 plot(x, y)
 
 m <- sum((x-mean(x))*(y-mean(y)))/sum((x-mean(x))^2)
+m
 b <- mean(y)-m*mean(x)
+b
 
 m*1.5+b
 
+####
+
 mod <- lm(y~x)
+mod
 summary(mod)
 
-ypredict <- predict(mod, data.frame(x=x))
-ypredict
+data.frame(x=x)
 
-plot(x,y,col="red",pch=16)
-lines(x, ypredict, col="black")
+ypred <- predict(mod, data.frame(x=x))
 
-#coefficiente de determinacion
-Rsq <- sum((ypredict-mean(y))^2)/sum((y-mean(y))^2)
+png("plot.png")
+ plot(x,y,col="red", pch=16)
+ lines(x, ypred)
+dev.off()
+
+Rsq <- sum((ypred-mean(y))^2)/sum((y-mean(y))^2)
 Rsq
 
 summary(mod)
-
-sqrt(0.1923)
-
-
-m <- 0.06576
-b <- 11.797555
-
-647.1*m+b
-
-data <- read.table("coches.txt", header = TRUE, sep="\t")
-data
-
